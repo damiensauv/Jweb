360ec8406aead1b9ac66c79545e2e6efd2770ea5
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-12-15 02:29:51 +0100

    re base od Project , new config, home screen("lol") , .gitignore , correction of some bug

---
+++ src/ORM/TomcatDB.java
@@ -0,0 +1,33 @@
+package ORM;
+
+import Entities.User;
+
+public  class  TomcatDB implements IDataBase {
+    TomcatDB()
+    {
+    }
+
+    public boolean     connectToDataBase()
+    {
+        /*
+        Not implemented
+         */
+        return false;
+    }
+
+    public boolean     add_user(User obj)
+    {
+        /*
+        Not implemented
+         */
+        return false;
+    }
+
+    public boolean  update_user(User obj)
+    {
+        /*
+        Not implemented
+         */
+        return false;
+    }
+}
